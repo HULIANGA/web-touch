8bcf83d48b7401a091c0d6d5a6a4bd460ec7e516
HULIANGA <835710406@qq.com>
2017-03-17 14:07:34 +0800

    虚拟键盘点击

--- 1.js
+++ 1.js
@@ -1,3 +1,7 @@
+function myFocus($obj){
+	$('.num-input').removeClass('active');
+	$obj.addClass('active');
+}
 $(function(){
 	// 切换主菜单
 	$('.slide-tab-box .tab-list .list-btn').on('click', function(){
@@ -5,6 +9,7 @@ $(function(){
 		$(this).addClass('active');
 		$('.slide-tab-item').hide();
 		$('.slide-tab-item[data-index='+$(this).attr('data-index')+']').show();
+		myFocus($('.slide-tab-item[data-index='+$(this).attr('data-index')+']').find('.num-input'));
 	})
 	// 切换会员菜单
 	$('.member-info-table .style-list li').on('click', function(){
@@ -21,32 +26,39 @@ $(function(){
 		$('.tab-ul-item[data-index='+$(this).attr('data-index')+']').show();
 	})
 	
-	
-    $('.num-input').on('click',function(e){
-        $(this).addClass('active');
-    })
+	// 虚拟键盘输入框
+	$('.num-input').click(function(){
+		myFocus($(this))
+	})
     $('.num-key-ul li').on('mousedown',function(e){
         var text=$('.num-input.active .text-num').text()+$(this).text();
         $(this).addClass('visited');
         $('.num-input.active .text-num').text(text);
+        $('.num-input.active .input-tip').hide();
     })
     $('.num-key-ul li').on('mouseup',function(e){
     	$('.num-key-ul li').removeClass('visited');
     });
 	$('.change-key li').on('mousedown',function(e){
         $(this).addClass('visited');
-        if($(this).hasClass('delet-btn')){
-            $('.num-input.active .text-num').text('');
+        if($(this).hasClass('delet-btn')){ // 删除
+            $('.num-input.active .text-num').text($('.num-input.active .text-num').text().slice(0,$('.num-input.active .text-num').text().length-1));
+            if(!$('.num-input.active .text-num').text()){
+            	$('.num-input.active .input-tip').show();
+            }
         }
-        if($(this).hasClass('ok-btn')){
+        if($(this).hasClass('ok-btn')){ // 确定
+        	var searchValue = $('.num-input.active .text-num').text();
             search();
         }
     })
     $('.change-key li').on('mouseup',function(e){
         $('.change-key li').removeClass('visited');
     })
-    //查询状态切换
+    // 查询状态切换
     $('.search-btn-box button').on('click',function(){
+    	$('.num-input.active .text-num').text('');
+    	$('.num-input.active .input-tip').text($(this).attr('data-placeholder')).show();
         $('.search-btn-box button').removeClass('visited');
         $(this).addClass('visited');
     })
@@ -54,8 +66,16 @@ $(function(){
     $('.using-point-btn').on('click',function(){
         $('#using_point_modal').modal('show');
     })
+    $('#using_point_modal').on('shown.bs.modal', function(){
+    	$('#using_point_modal .num-input .text-num').text('');
+        $('#using_point_modal .num-input .input-tip').show();
+        myFocus($('#using_point_modal .num-input').eq(0));
+    })
     $('#using_point_modal .cancel-btn').on('click',function(){
         $('#using_point_modal').modal('hide');
+    })
+    $('#using_point_modal').on('hidden.bs.modal', function(){
+        myFocus($('.pos-constom-content .num-input'));
     })
     //使用优惠券
     $('.using-coupon-btn').on('click',function(){
